9052387347345e7439a993c555db9b7770b259d1
Dan Slimmon <dan.slimmon@gmail.com>
2013-06-28 21:21:04 +0000

    New version (with is_up())

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='chube',
-        version='0.1.11',
+        version='0.1.12',
         author='Exosite',
         author_email='danslimmon@exosite.com',
         packages=['chube'],
