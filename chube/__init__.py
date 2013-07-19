f7a8ad49168edb2ef9558285dfc204f081810260
Dan Slimmon <dan.slimmon@gmail.com>
2013-07-19 22:27:17 +0000

    Added usage message and version flag.

--- chube/__init__.py
+++ chube/__init__.py
@@ -11,7 +11,7 @@ from .stackscript import Stackscript, StackscriptInput
 from .linode_obj import Linode, Disk, Config, IPAddress
 from .nodebalancer import Nodebalancer
 from .dns import Domain
-__version__ = "0.1.15"
+CHUBE_VERSION = "0.1.15"
 
 def load_chube_config():
     """Loads the Chube config from ~/.chube."""
